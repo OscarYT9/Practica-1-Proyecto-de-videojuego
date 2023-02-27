8b4cbbd3dae45ac5cc20d9225022be456501ac2b
Oscar V.R <ferosre@gmail.com>
2023-02-27 18:35:10 +0000

    Update main.py

--- Practica1-20230215/materiales/main.py
+++ Practica1-20230215/materiales/main.py
@@ -19,10 +19,26 @@ def run(path):
             personajes.append(personaje)
 
     #TODO: Implement simulation here
+    for personaje in personajes:
+        nombres = []
+        nombres.append(personaje.name)
 
-    for i in range(0,31):
-        while len(personajes)>1:
-            combat(personajes)
+    #while len(pjs)>1:
+    combat(nombres)
+
+
+def create_character(name, life, strength, protection, fury):
+    weapon, armor, shield = None, None, None
+    return Warrior(name, life, strength, protection, fury, weapon, armor, shield)
+
+def create_character1(name, life, strength, protection, mana):
+    weapon, armor = None, None
+    return Mage(name, life, strength, protection, mana, weapon, armor)
+
+def create_character2(name, life, strength, protection, mana):
+    weapon, armor = None, None
+    return Priest(name, life, strength, protection, mana, weapon, armor)
+
 
 def parse_params(params):
 
@@ -30,15 +46,19 @@ def parse_params(params):
     if params[0].lower() == "warrior":
         fury = int(params[5])
         weapon, armor, shield = None, None, None
-        print ("Create Warrior  [todo]")
+        #print ("Create Warrior  [todo]")
+        return create_character(name, life, strength, protection, fury)
+
     elif params[0].lower() == "mage":
         mana = int(params[5])
         weapon, armor = None, None
-        print ("Create Mage [todo]")
+        #print ("Create Mage [todo]")
+        return create_character1(name, life, strength, protection, mana)
     elif params[0].lower() == "priest":
         mana = int(params[5])
         weapon, armor = None, None
-        print ("Create Priest [todo]")
+        #print ("Create Priest [todo]")
+        return create_character2(name, life, strength, protection, mana)
     else:
         raise ValueError("Avatar '{}' is not valid".format(params[0]))
     
@@ -46,4 +66,3 @@ def parse_params(params):
 if __name__ == "__main__":
 
     run(sys.argv[1])
-
